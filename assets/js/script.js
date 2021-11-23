e3708c5fc1fa367fbd927455bb26c6ddd99b81aa
christi marchetti <christimarchetti@gmail.com>
2021-11-22 22:02:54 -0500

    contact me flexbox and refactor carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,38 @@ var index = 0;
 
 
 function show_project(i) {
-    index += 1;
+    index += i;
+    console.log(index)
 
     var projects = document.getElementsByClassName("project");
+    //if user is on last project and clicks next, the user will be sent to first project
+    if(index > projects.length - 1) {
+        index = 0;
+    }
+        //if user is on first project and clicks previous, the user will be sent to last project
+        if(index < 0) {
+        index = projects.length - 1 ;
+    }
 
     var dots = document.getElementsByClassName("dot");
 
     //blocks projects from showing on screen
-    for (i = 0;i < projects.length;i++)
+    for (i = 0;i < projects.length;i++) {
     projects[i].style.display = "none"
+    }
 
     //turns the dot darker corresponding with the project displayed
-    for (i = 0;i < dots.length;i++)
-    dots[1].className = dots[i].className.replace("active", "")
-
-    //if user is on last project and clicks next, the user will be sent to first project
-    if(index > projects.length - 1)
-    index = 0;
-
-    //if user is on first project and clicks previous, the user will be sent to last project
-    if(index < 0)
-    index = projects.length - 1 ;
+    for (i = 0;i < dots.length;i++) {
+    dots[i].className = dots[i].className.replace("active", "")
+    }
+    dots[index].classList.add('active')
 
     projects[index].style.display = "block";
-    dots[index].className += "active";
 }
 
 show_project(index);
+
+function send() {
+    emailjs.init("user_HxFKMlHEFrDRwR3g925np");
+} 
+send();
